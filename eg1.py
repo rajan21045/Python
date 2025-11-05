464d762250c0b91c87de48650751c0d8ff1ea38c
Rajan Poudel <raajan.works@gmail.com>
2025-11-05 11:47:37 +0545

    Add files via upload

---
+++ eg1.py
@@ -0,0 +1,5 @@
+import os
+# os.mkdir('newdirectory')
+# os.makedirs('parent_directory/child_directory')
+# os.removedirs('parent_directory/child_directory')
+os.rename('parent_directory', 'directorynew')
