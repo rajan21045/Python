de9c59917931918694bfef4eb9abb99019fd4e54
Rajan Poudel <raajan.works@gmail.com>
2025-11-06 12:42:13 +0545

    Add files via upload

---
+++ pandas1.py
@@ -0,0 +1,45 @@
+import pandas as pd
+
+# create a series
+s = pd.Series([1,2,3,4,5])
+print("Series:\n", s)
+
+# create a DataFrame
+data = {
+    'A': [1, 2, 3, 4],
+    'B': [5, 6, 7, 8],
+    'C': [9, 10, 11, 12]
+}
+df = pd.DataFrame(data)
+print("\nDataFrame:\n", df)
+
+dd = {
+    'Name': ['Subesh', 'Smith', 'Rajan', 'Ritesh', 'Amir'],
+    'Age': [25, 27, 25, 26, 25],
+    'City': ['Bus-Park', 'Cancer-Gate', 'Kshetrapur', 'Lakeside', 'Newroad']
+}
+df1 = pd.DataFrame(dd)
+print("\nDataFrame df1:\n", df1)
+
+# Accessing specific columns from df1 called 'Age
+age_column = df1['Age']
+print("Age Column:\n",age_column)
+
+# Accessing specific columns from df1 called 'Name' and 'City'
+print("Age Colum And City Column:\n", df1[['Name', 'City']])
+
+# Selecting rows by index position
+first_row = df1.iloc[0]
+print("\nFirst Row:\n", first_row)
+
+# Selecting multiple rows by index position
+first_two_rows = df.loc[0:1]
+print("\nFirst Two Rows:\n", first_two_rows)
+
+# Adding new column 'Salary'
+df1['Salary'] = [50000, 60000, 55000, 52000, 58000]
+print("\nDataFrame df1 after adding Salary column:\n", df1)
+
+# Filtering Data: Rows where Age > 25
+
+
