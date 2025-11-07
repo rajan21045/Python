c29e39dd6ba3491770e6d0b14baaf1ee0f10e73f
Rajan Poudel <raajan.works@gmail.com>
2025-11-07 18:35:48 +0545

    Delete FindGreaterOrEqualNumber.py

--- FindGreaterOrEqualNumber.py
+++
@@ -1,9 +0,0 @@
-c = int(input("Enter first number: "))
-d = int(input("Enter second number: "))
-if c > d:
-    print(f"{c} is greater than {d}.")
-elif c < d:
-    print(f"{d} is greater than {c}.")
-else:
-    print(f"{c} and {d} are equal.")
- 
