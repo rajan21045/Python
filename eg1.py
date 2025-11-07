4f7dd200ec0e526fa0a5e21e9599fc8ead690e06
Rajan Poudel <raajan.works@gmail.com>
2025-11-07 19:07:15 +0545

    Delete eg1.py

--- eg1.py
+++
@@ -1,5 +0,0 @@
-import os
-# os.mkdir('newdirectory')
-# os.makedirs('parent_directory/child_directory')
-# os.removedirs('parent_directory/child_directory')
-os.rename('parent_directory', 'directorynew')
