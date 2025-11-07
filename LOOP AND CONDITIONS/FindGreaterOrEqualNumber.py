fb663435eeb848afc2ff2d0210cdc8eca013ad0d
Rajan Poudel <raajan.works@gmail.com>
2025-11-07 18:35:26 +0545

    Add files via upload

---
+++ LOOP AND CONDITIONS/FindGreaterOrEqualNumber.py
@@ -0,0 +1,8 @@
+c = int(input("Enter first number: "))
+d = int(input("Enter second number: "))
+if c > d:
+    print(f"{c} is greater than {d}.")
+elif c < d:
+    print(f"{d} is greater than {c}.")
+else:
+    print(f"{c} and {d} are equal.")
