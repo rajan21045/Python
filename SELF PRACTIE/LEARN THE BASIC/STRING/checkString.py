ea395a3393614760bb304890471d835b500b84a8
Rajan Poudel <raajan.works@gmail.com>
2025-12-07 20:04:04 +0545

    Add files via upload

---
+++ SELF PRACTIE/LEARN THE BASIC/STRING/checkString.py
@@ -0,0 +1,7 @@
+txt = "The Best Things In Life Are Free!"
+
+print("free" in txt)
+# returns True because "free" is found in the string
+
+print("expensive" not in txt)
+# returns True because "expensive" is NOT found in the string
---
+++ SELF PRACTIE/LEARN THE BASIC/STRING/stringConcatenation.py
@@ -0,0 +1,9 @@
+a = "Rajan Poudel. I Love Programming."
+b = " I Love Python Programming Language."
+
+c= a + b
+print(c)
+# prints the concatenated string: "Rajan Poudel. I Love Programming. I Love Python Programming Language."
+d = a + " " + b
+print(d)
+# prints the concatenated string with space in between: "Rajan Poudel. I Love Programming.  I Love Python Programming Language."
