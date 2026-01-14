a3abfdc8ac2230ba8eab30a65e7857677208eba0
Rajan Poudel <raajan.works@gmail.com>
2026-01-14 19:22:51 +0545

    Add files via upload

---
+++ SELF PRACTIE/LEARN THE BASIC/LOOP/whileLoop.py
@@ -0,0 +1,28 @@
+i = 1
+while i< 6:
+    print(i)
+    i+=1
+
+
+j = 1
+print("\n")
+while j< 6:
+    print(j)
+    if j == 2:
+        break
+    j+=1
+
+k=0
+print("\n")
+while k< 6:
+    k+=1
+    if k == 2:
+        continue
+    print(k)
+
+l =1
+while l <6:
+    print(l)
+    l+=1
+else:
+    print("l is no longer less than 6")
