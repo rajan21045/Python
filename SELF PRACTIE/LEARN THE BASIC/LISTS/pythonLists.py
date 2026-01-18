fc9932b601bd095fd2be73ea603e591f54ad1df3
Rajan Poudel <raajan.works@gmail.com>
2026-01-18 21:49:30 +0545

    Add files via upload

---
+++ SELF PRACTIE/LEARN THE BASIC/LISTS/pythonLists.py
@@ -0,0 +1,22 @@
+firstList = ["Rajan", "Bruce", "Batman"]
+print(len(firstList))
+print("\n")
+
+list1 = ["apple", "banana", "cherry"]
+print(list1)
+print("\n")
+
+list2 = [1, 5, 7, 9, 3]
+print(list2)
+print("\n")
+
+list3 = [True, False, False]
+print(list3)
+print("\n")
+
+list4 = ["abc", 34, True, 40, "male"]
+print(type(list4))
+print(type(firstList))
+
+thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist)
