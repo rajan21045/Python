84317fb90f91b6e76ad16868dcf9e535245a6456
Rajan Poudel <raajan.works@gmail.com>
2026-01-31 22:43:12 +0545

    Update pythonListsItems.py

--- SELF PRACTIE/LEARN THE BASIC/LISTS/pythonListsItems.py
+++ SELF PRACTIE/LEARN THE BASIC/LISTS/pythonListsItems.py
@@ -4,7 +4,7 @@ print(f"1 Index Value Is: {thislist[1]}")
 
 print(thislist[-1]) #-1 refers to the last item
 print(thislist[-2]) # -2 refers to the second last item etc.
-
+ 
 thislist1 = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
 print(thislist1[2:5])
 print(thislist1[2:])
