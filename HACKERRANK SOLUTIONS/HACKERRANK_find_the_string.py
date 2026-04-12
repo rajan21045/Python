2b1cd8e027c78fe16c10ce9bcb14321bce82d9ff
Rajan Poudel <raajan.works@gmail.com>
2026-04-12 07:25:35 +0545

    Add files via upload

---
+++ HACKERRANK SOLUTIONS/HACKERRANK_find_the_string.py
@@ -0,0 +1,67 @@
+'''
+In this challenge, the user enters a string and a substring. You have to print the number of times that the substring occurs in the given string. String traversal will take place from left to right, not from right to left.
+
+NOTE: String letters are case-sensitive.
+
+Input Format
+
+The first line of input contains the original string. The next line contains the substring.
+
+Output Format
+
+Output the integer number indicating the total number of occurrences of the substring in the original string.
+
+Sample Input
+
+ABCDCDC
+CDC
+
+Sample Output
+
+2
+
+Concept
+
+There are a couple of new concepts:
+In Python, the length of a string is found by the function len(s), where s is the string.
+To traverse through the length of a string, use a for loop:
+
+for i in range(0, len(s)):
+    print (s[i])
+
+A range function is used to loop over some length:
+
+range (0, 5)
+Here, the range loops over to . is excluded.
+'''
+
+# ========================
+#       Information
+# ========================
+
+# Direct Link: https://www.hackerrank.com/challenges/find-a-string/problem
+# Difficulty: Easy
+# Max Score: 10
+# Language: Python
+
+# ========================
+#         Solution
+# ========================
+
+def count_substring(string, sub_string):
+    count = 0
+
+    i = string.find(sub_string)
+
+    while i != -1:
+        count += 1
+        i = string.find(sub_string, i+1)
+    return count
+    
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
