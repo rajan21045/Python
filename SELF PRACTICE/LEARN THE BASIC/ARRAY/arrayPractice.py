cf9b5c67dfba9c7e0149f458a65e52e3462e33b3
Rajan Poudel <raajan.works@gmail.com>
2026-04-26 20:42:16 +0545

    Add files via upload

---
+++ SELF PRACTICE/LEARN THE BASIC/ARRAY/arrayPractice.py
@@ -0,0 +1,17 @@
+# Arrays are used to store multiple values in one single variable:
+myArray = ['Rajan', 'Prabin', 'Ashok', 'Sasin']
+
+# What is an Array?
+# An array is a special variable, which can hold more than one value at a time.
+
+#Access the Elements of an Array
+# You refer to an array element by referring to the index number.
+# Example
+# Get the value of the first array item:
+x = myArray[0]
+print(x)
+
+# The Length of an Array
+# Use the len() method to return the length of an array (the number of elements in an array).
+# Example
+print(f"The Length of The Array Named myArray is: {len(myArray)}")
