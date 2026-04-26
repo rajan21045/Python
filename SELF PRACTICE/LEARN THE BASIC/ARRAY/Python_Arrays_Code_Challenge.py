572cc6e9376bffb0461d83221350a16baf87116b
Rajan Poudel <raajan.works@gmail.com>
2026-04-26 20:44:07 +0545

    Add files via upload

---
+++ SELF PRACTICE/LEARN THE BASIC/ARRAY/Python_Arrays_Code_Challenge.py
@@ -0,0 +1,24 @@
+# Python Arrays Code Challenge Link: https://www.w3schools.com/python/python_challenges_arrays.asp
+
+# Python Arrays Code Challenge
+# Challenge: Arrays
+# Test your understanding of Python arrays by completing a small coding challenge.
+
+# Instructions
+# Inside the editor, complete the following steps:
+# Create a list called cars with the values "Ford", "Volvo", "BMW"
+# Print the first item in the list
+# Change the second item to "Toyota"
+# Print the list
+
+# Create a list
+cars = ["Ford", "Volvo", "BMV"]
+
+# Print the first item
+print(cars[0])
+
+# Change the second item to "Toyota"
+cars[1] = "Toyota"
+# Print the list
+print(cars)
+
